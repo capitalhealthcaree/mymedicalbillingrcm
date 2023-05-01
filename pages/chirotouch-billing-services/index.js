d53aa239f2b1b801ac97ba0706b44aed7ea75ef5
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-05-01 05:54:37 -0700

    move content

--- pages/chirotouch-billing-services/index.js
+++ pages/chirotouch-billing-services/index.js
@@ -38,6 +38,68 @@ const RCM = () => {
               ChiroTouch clients can access their records and manage their
               accounts through the vendor's MyChiroTouch offer.
             </p>
+            <div>
+              <h2>Billing Services using ChiroTouch Software Process Flow</h2>
+              <p>
+                As one of the leading ChiroTouch billing services within the
+                United States and internationally, Advance Healthcare is one of
+                the best. To meet the varying needs of our clients, we
+                streamline our billing services using ChiroTouch software. In
+                order to ensure quality service, we alter and modify the flow
+                according to the needs of the client. The following is a
+                description of the unique process flow by which ChiroTouch
+                billing services are provided -
+              </p>
+              <ul>
+                <li>
+                  <span className="fw-bold">Customized Integration</span>: After
+                  discussions with our customers and service level agreements,
+                  we collect and store their personal information in our secure
+                  database. Following this, our staff ensures that they
+                  understand the customer's requirements completely. In order to
+                  proceed further, a customized version of our software is
+                  created. In the end, all protocols and security measures need
+                  to be followed.
+                </li>
+                <li>
+                  <span className="fw-bold">Access to Data</span>: Upon
+                  integration with the customer's system, our staff will be able
+                  to access all patient records. The billing process includes a
+                  cross-check of each patient's billing. It is our
+                  responsibility to review every insurance claim made, as well
+                  as any denials or rejections. Resubmit your insurance claim
+                  after understanding the issues.
+                </li>
+                <li>
+                  <span className="fw-bold"> Collection Capture</span>: In order
+                  to access the application, patients can use a barcode or PIN
+                  code, a swipe card or a fingerprint scanner to log in.The
+                  result is that your desk staff will be freed from the mundane
+                  and time-consuming tasks associated with customer service.
+                </li>
+                <li>
+                  <span className="fw-bold">Provider Dashboard</span>
+                  :Chiropractic medical billing utilizes a framework and billing
+                  codes. The purpose of codes is to provide services such as
+                  insurance processing and account management. As soon as the
+                  procedure is set up, a trial is run to ensure its accuracy..
+                  Afterwards, our team initiates the collection process, thereby
+                  improving your Revenue Management Cycle (RMC).
+                </li>
+                <li>
+                  <span className="fw-bold"> Reporting</span>: Our customers
+                  receive constant updates through a self-managing dashboard or
+                  through email or any other medium that they choose. Every
+                  week, we provide customers with an update on the number of
+                  insurance claims made on a daily basis, the number of
+                  rejections or errors that have occurred, the number of bills
+                  cleared, and the number of backlogs. With ChiroTouch software,
+                  we provide billing services that ensure accurate and timely
+                  payment. Our experts provide our customers with a report
+                  detailing the total amount received and the revenue generated.
+                </li>
+              </ul>
+            </div>
           </div>
 
           <div className="row mt-5">
@@ -61,6 +123,7 @@ const RCM = () => {
                     management capabilities. HIPAA compliance is ensured
                     throughout the entire system. Among other benefits are:
                   </span>
+
                   <ul>
                     <li>
                       <span className="fw-bold"> Front Desk Application</span>:
@@ -210,66 +273,6 @@ const RCM = () => {
                 </form>
               </div>
             </div>
-          </div>
-          <div>
-            <h2>Billing Services using ChiroTouch Software Process Flow</h2>
-            <p>
-              As one of the leading ChiroTouch billing services within the
-              United States and internationally, Advance Healthcare is one of
-              the best. To meet the varying needs of our clients, we streamline
-              our billing services using ChiroTouch software. In order to ensure
-              quality service, we alter and modify the flow according to the
-              needs of the client. The following is a description of the unique
-              process flow by which ChiroTouch billing services are provided -
-            </p>
-            <ul>
-              <li>
-                <span className="fw-bold">Customized Integration</span>: After
-                discussions with our customers and service level agreements, we
-                collect and store their personal information in our secure
-                database. Following this, our staff ensures that they understand
-                the customer's requirements completely. In order to proceed
-                further, a customized version of our software is created. In the
-                end, all protocols and security measures need to be followed.
-              </li>
-              <li>
-                <span className="fw-bold">Access to Data</span>: Upon
-                integration with the customer's system, our staff will be able
-                to access all patient records. The billing process includes a
-                cross-check of each patient's billing. It is our responsibility
-                to review every insurance claim made, as well as any denials or
-                rejections. Resubmit your insurance claim after understanding
-                the issues.
-              </li>
-              <li>
-                <span className="fw-bold"> Collection Capture</span>: In order
-                to access the application, patients can use a barcode or PIN
-                code, a swipe card or a fingerprint scanner to log in.The result
-                is that your desk staff will be freed from the mundane and
-                time-consuming tasks associated with customer service.
-              </li>
-              <li>
-                <span className="fw-bold">Provider Dashboard</span>:Chiropractic
-                medical billing utilizes a framework and billing codes. The
-                purpose of codes is to provide services such as insurance
-                processing and account management. As soon as the procedure is
-                set up, a trial is run to ensure its accuracy.. Afterwards, our
-                team initiates the collection process, thereby improving your
-                Revenue Management Cycle (RMC).
-              </li>
-              <li>
-                <span className="fw-bold"> Reporting</span>: Our customers
-                receive constant updates through a self-managing dashboard or
-                through email or any other medium that they choose. Every week,
-                we provide customers with an update on the number of insurance
-                claims made on a daily basis, the number of rejections or errors
-                that have occurred, the number of bills cleared, and the number
-                of backlogs. With ChiroTouch software, we provide billing
-                services that ensure accurate and timely payment. Our experts
-                provide our customers with a report detailing the total amount
-                received and the revenue generated.
-              </li>
-            </ul>
           </div>
         </div>
       </div>
